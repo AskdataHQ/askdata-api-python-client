8379d74c6eba4fc1cf872ef2b24d3518158ca98a
Luca Sarcona <>
2020-10-13 16:26:07 +0200

    all_info=True

--- askdata/dataset.py
+++ askdata/dataset.py
@@ -276,7 +276,7 @@ class Dataset():
         dataset_id = self.get_id_dataset_by_name(dataset_name)
         if(dataset_id!=[]):
             logging.info("DATASET ID {}".format(dataset_id))
-            settings_dataset = self.__get_dataset_settings_info(dataset_id[0])
+            settings_dataset = self.__get_dataset_settings_info(dataset_id[0], True)
         else:
             logging.info("DATASET ID {}")
             dataset_id, settings_dataset = self.__create_dataset_df(dataset_name)
