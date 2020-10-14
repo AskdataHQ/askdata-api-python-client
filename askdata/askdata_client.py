f98bd5ef1b058ca09105441cee2a4404909c04e8
Luca Sarcona <>
2020-10-14 18:34:48 +0200

    Change logs

--- askdata/askdata_client.py
+++ askdata/askdata_client.py
@@ -51,7 +51,7 @@ class Agent(Insight, Channel, Catalog, Dataset):
             "Content-Type": "application/json",
             "Authorization": "Bearer" + " " + self._token
         }
-        logging.info("ID AGENTS {}".format(self.df_agents))
+
         try:
             if slug != '':
                 agent = self.df_agents[self.df_agents['slug'] == slug.lower()]
@@ -60,7 +60,6 @@ class Agent(Insight, Channel, Catalog, Dataset):
             else:
                 agent = self.df_agents[self.df_agents['name'] == agent_name]
 
-            logging.info("AGENT {}".format(agent))
             self._agentId = agent.iloc[0]['id']
             self._domain = agent.iloc[0]['domain']
             self._language = agent.iloc[0]['language']
